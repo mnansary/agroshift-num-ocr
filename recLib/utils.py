ac850223ff2e7d0faff452a2e82ca9362bd44f9d
mnansary <silentassasin0111@gmail.com>
2022-11-25 19:37:55 +0600

    tfrecord creation for reclib

--- recLib/utils.py
+++ recLib/utils.py
@@ -119,7 +119,7 @@ def quasicrystal(height, width):
             pixels[kw, kh] = c  # grayscale
     return np.array(image.convert("RGB"))
 
-    #---------------------wrapper
+#---------------------wrapper
 def paper_noise(img):
     if random_exec(weights=[0.75,0.25]):
         h,w=img.shape
@@ -143,3 +143,83 @@ def paper_noise(img):
     if random_exec():
         img=cv2.GaussianBlur(img,(3,3),0)
     return img
+#---------------------------------------------------------------
+# image store utils
+#------------------------------------------------------------------------
+def padWordImage(img,pad_loc,pad_dim,pad_val):
+    '''
+        pads an image with white value
+        args:
+            img     :       the image to pad
+            pad_loc :       (lr/tb) lr: left-right pad , tb=top_bottom pad
+            pad_dim :       the dimension to pad upto
+            pad_val :       the value to pad 
+    '''
+    
+    if pad_loc=="lr":
+        # shape
+        h,w,d=img.shape
+        # pad widths
+        pad_width =pad_dim-w
+        # pads
+        pad =np.ones((h,pad_width,3))*pad_val
+        # pad
+        img =np.concatenate([img,pad],axis=1)
+    else:
+        # shape
+        h,w,d=img.shape
+        # pad heights
+        if h>= pad_dim:
+            return img 
+        else:
+            pad_height =pad_dim-h
+            # pads
+            pad =np.ones((pad_height,w,3))*pad_val
+            # pad
+            img =np.concatenate([img,pad],axis=0)
+    return img.astype("uint8")    
+#---------------------------------------------------------------
+def correctPadding(img,dim,pvalue=255):
+    '''
+        corrects an image padding 
+        args:
+            img     :       numpy array of single channel image
+            dim     :       tuple of desired img_height,img_width
+            pvalue  :       the value to pad
+        returns:
+            correctly padded image
+    '''
+    img_height,img_width=dim
+    mask=0
+    # check for pad
+    h,w,d=img.shape
+    
+    w_new=int(img_height* w/h) 
+    img=cv2.resize(img,(w_new,img_height))
+    h,w,d=img.shape
+    
+    if w > img_width:
+        # for larger width
+        h_new= int(img_width* h/w) 
+        img=cv2.resize(img,(img_width,h_new))
+        # pad
+        img=padWordImage(img,
+                     pad_loc="tb",
+                     pad_dim=img_height,
+                     pad_val=pvalue)
+        mask=img_width
+
+    elif w < img_width:
+        # pad
+        img=padWordImage(img,
+                    pad_loc="lr",
+                    pad_dim=img_width,
+                    pad_val=pvalue)
+        mask=w
+    
+    # error avoid
+    img=cv2.resize(img,(img_width,img_height))
+    msk=np.zeros((img_height,img_width))
+    msk[:,mask:]=255
+    return img,msk 
+#------------------------------------------------------------------------
